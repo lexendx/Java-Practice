1cd22717d47c8a499365fc4acb4111bbd49bb2c1
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-11-30 01:46:49 +0530

    solviing more problems

---
+++ EvenNo.java
@@ -0,0 +1,13 @@
+
+public class EvenNo {
+    public static void main(String[] args) {
+        int i=1;
+      
+        while(i<=0){
+            if(i%2==0){
+                System.out.println("Even Number are" + i);
+            }
+            i++;
+        }
+    }
+}
