d2372563040913771b374dd4bbe3f2820428a984
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-01 00:27:25 +0530

    While Statement program added

---
+++ While Programs/Program1.java
@@ -0,0 +1,15 @@
+
+
+public class Program1 {
+    public static void main(String[] args) {
+        int i = 1;
+        int count = 0;
+        while(i<=16){
+            if(i%3==0){
+                System.out.println(i);
+                count++;
+            }i++;
+        }System.out.println(count);
+    }
+    
+}
